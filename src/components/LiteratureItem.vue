1fa3805a959205be21b96f458d3365006599aafc
xkazuma <kusu@mil.doshisha.ac.jp>
2022-07-19 19:02:48 +0900

    UI for searching books with API

--- src/components/LiteratureItem.vue
+++ src/components/LiteratureItem.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="card h-100 literature-item">
+  <div class="card bg-light h-100 literature-item">
     <div class="row g-0">
       <div class="col-md-4">
         <img class="card-img-top img-thumbnail" v-bind:alt="[book.title]" v-bind:src=[book.image]>
       </div>
+
       <div class="col-md-8">
-        <div class="card-body">
+        <div v-if="compInBasket === false"
+             class="card-body" >
           <h4 class="card-title">{{book.title}}</h4>
+
           <h5 class="card-text">{{book.author}}</h5>
           <p class="card-text">{{book.desc}}</p>
+        </div>
+        <div v-else
+             class="card-body" >
+          <h6 class="card-title">{{book.title}}</h6>
         </div>
       </div>
     </div>
@@ -16,19 +23,44 @@
 </template>
 
 <script>
+
 export default {
+
   name: "literature-item",
   props: {
     book: {
-      author: String,
-      title: String,
-      image: String,
-      desc: String,
-      isbn: String,
+      author      : String,
+      title       : String,
+      image       : String,
+      description : String,
+      isbn        : String,
+      exists      : Boolean,
+    },
+    inBasket     : Boolean,
+    compInBasket : Boolean,
+  },
+
+  setup() { },
+
+  watch : {
+
+    buttonText(buttonText) {
+
+      return buttonText;
+
+    },
+
+    buttonClass(buttonClass) {
+
+      return buttonClass;
+
     }
   },
-  data() {},
-  computed: {}
+
+  computed: { },
+
+  methods: { },
+
 }
 </script>
 
